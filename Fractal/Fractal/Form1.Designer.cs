f78b3f9728714cf256e88f40d17b2f0fc7915880
Tanjeer <tanjeer@asabd.org>
2017-05-03 16:38:26 +0600

    committed for the first time with full source code

---
+++ Fractal/Fractal/Form1.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Fractal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.drawAreaPanel = new System.Windows.Forms.Panel();
+            this.startButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.iterationLabel = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // drawAreaPanel
+            // 
+            this.drawAreaPanel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.drawAreaPanel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.drawAreaPanel.Location = new System.Drawing.Point(50, 58);
+            this.drawAreaPanel.Name = "drawAreaPanel";
+            this.drawAreaPanel.Size = new System.Drawing.Size(840, 440);
+            this.drawAreaPanel.TabIndex = 0;
+            this.drawAreaPanel.Paint += new System.Windows.Forms.PaintEventHandler(this.drawAreaPanel_Paint);
+            this.drawAreaPanel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.drawAreaPanel_MouseClick);
+            // 
+            // startButton
+            // 
+            this.startButton.Enabled = false;
+            this.startButton.Location = new System.Drawing.Point(50, 29);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(75, 23);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(351, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Iteration(s):";
+            // 
+            // stopButton
+            // 
+            this.stopButton.Enabled = false;
+            this.stopButton.Location = new System.Drawing.Point(131, 29);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(75, 23);
+            this.stopButton.TabIndex = 3;
+            this.stopButton.Text = "Stop";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // iterationLabel
+            // 
+            this.iterationLabel.AutoSize = true;
+            this.iterationLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.iterationLabel.ForeColor = System.Drawing.Color.Blue;
+            this.iterationLabel.Location = new System.Drawing.Point(410, 34);
+            this.iterationLabel.Name = "iterationLabel";
+            this.iterationLabel.Size = new System.Drawing.Size(15, 15);
+            this.iterationLabel.TabIndex = 4;
+            this.iterationLabel.Text = "0";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(279, 32);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(35, 20);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(234, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(41, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Speed:";
+            // 
+            // clearButton
+            // 
+            this.clearButton.Enabled = false;
+            this.clearButton.Location = new System.Drawing.Point(815, 30);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 7;
+            this.clearButton.Text = "Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(986, 512);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.iterationLabel);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.drawAreaPanel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Fractal";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel drawAreaPanel;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button stopButton;
+        private System.Windows.Forms.Label iterationLabel;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearButton;
+    }
+}
+
---
+++ Fractal/Fractal/Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fractal
+{
+    public partial class Form1 : Form
+    {
+        private Point _xPoint = new Point(100,400);
+        private Point _yPoint = new Point(700,400);
+        private Point _zPoint = new Point(400,50);
+        private Point _currentPoint = new Point();
+        private Random _random = new Random();
+        private bool _isStopped = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void drawAreaPanel_MouseClick(object sender, MouseEventArgs e)
+        {
+            _currentPoint = new Point(e.X, e.Y);
+            CreateDot(drawAreaPanel, _currentPoint);
+            startButton.Enabled = true;
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            stopButton.Enabled = true;
+            clearButton.Enabled = false;
+            startButton.Enabled = false;
+            var thread = new Thread(new ThreadStart(CreateFractal));
+            thread.Start();
+        }
+
+        private void drawAreaPanel_Paint(object sender, PaintEventArgs e)
+        {
+            using (var g = e.Graphics)
+            {
+                CreateDot(g, _xPoint);
+                CreateDot(g, _yPoint);
+                CreateDot(g, _zPoint);
+            }
+        }
+
+        private void stopButton_Click(object sender, EventArgs e)
+        {
+            _isStopped = true;
+            stopButton.Enabled = false;
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            drawAreaPanel.Refresh();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CreateFractal()
+        {
+            using (var g = drawAreaPanel.CreateGraphics())
+            {
+                var i = 1;
+                while (!_isStopped)
+                {
+                    iterationLabel.Text = (i++).ToString("##,###");
+                    _currentPoint = GetMiddlePoint(_currentPoint, GetRandomPoint());
+                    CreateDot(g, _currentPoint);
+                    Thread.Sleep((int)(1000 / numericUpDown1.Value));
+                }
+            }
+            stopButton.Enabled = false;
+            startButton.Enabled = true;
+            clearButton.Enabled = true;
+        }
+
+        private void CreateDot(Control control, Point point)
+        {
+            using (var g = control.CreateGraphics())
+            {
+                g.FillRectangle((Brush)Brushes.Yellow, point.X, point.Y, 3, 3);
+            }
+        }
+
+        private void CreateDot(Graphics g, Point point)
+        {
+            g.FillRectangle((Brush)Brushes.Yellow, point.X, point.Y, 3, 3);
+        }
+        
+
+        private Point GetRandomPoint()
+        {
+            var randNumber = _random.Next(1, 7);
+            switch (randNumber)
+            {
+                case 1:
+                case 2:
+                    return _xPoint;
+                case 3:
+                case 4:
+                    return _yPoint;
+                case 5:
+                case 6:
+                    return _zPoint;
+                default:
+                    return _xPoint;
+            }
+        }
+
+        private Point GetMiddlePoint(Point firstPoint, Point secondPoint)
+        {
+            return new Point((firstPoint.X + secondPoint.X)/2, (firstPoint.Y + secondPoint.Y)/2);
+        }
+    }
+}
